fbab562289863743491dd0c829124b6be6072b10
Paulo Henrique Grancieri <paulohenriquegrancieri@gmail.com>
2024-07-24 22:45:00 -0300

    finish login form design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Import the FormsModule
 
 import { routes } from './app.routes';
+
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    FormsModule // Add FormsModule to imports
   ],
   providers: [],
   bootstrap: []
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 // login.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, NgForm, NgModel} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,4 +10,31 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./login.component.css'],
   imports: [CommonModule, FormsModule]
 })
-export class LoginComponent { }
+export class LoginComponent {
+  passwordInputType: string = 'password';
+  passwordVisibilityIcon: string = 'assets/images/eye-icon.png'; // Default eye icon
+
+  onBlur(control: NgModel) {
+    control.control.markAsTouched();
+  }
+
+  onSubmit(loginForm: NgForm) {
+    if (loginForm.valid) {
+      // Handle valid form submission
+      console.log('Form Submitted', loginForm.value);
+    } else {
+      // Handle invalid form submission
+      console.log('Form Invalid');
+    }
+  }
+
+  togglePasswordVisibility() {
+    if (this.passwordInputType === 'password') {
+      this.passwordInputType = 'text';
+      this.passwordVisibilityIcon = 'assets/images/eye-off-icon.png'; // Eye off icon
+    } else {
+      this.passwordInputType = 'password';
+      this.passwordVisibilityIcon = 'assets/images/eye-icon.png'; // Eye icon
+    }
+  }
+}
